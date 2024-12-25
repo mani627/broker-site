ae68af40aa53687a2296a2bcced216ced3b37361
Manikandan R <mani@example.com>
2024-12-25 20:49:48 +0530

    updated_build_optimisation

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,29 @@ export default defineConfig({
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
+  build: {
+    // Minification: Using esbuild for fast minification
+    minify: 'esbuild', // Alternatively, you can use 'terser' for a smaller but slower build
+
+    // Enable tree-shaking (default behavior)
+    // Vite uses ES Modules (ESM) by default, enabling tree-shaking automatically
+    treeshake: true, 
+
+    // Code Splitting: Split the vendor libraries into a separate chunk
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Splitting vendor libraries from application code
+            return 'vendor'; // All node_modules will go into 'vendor.js'
+          }
+        },
+      },
+    },
+
+    // Enable source maps only for development
+   // sourcemap: process.env.NODE_ENV === 'production' ? false : true,
+  },
   
   server: {
     hmr: true,
